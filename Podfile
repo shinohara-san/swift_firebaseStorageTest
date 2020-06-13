ace77c9523ba66a062925a3bc4f1115dfe0c1190
shinohara-san <someiyoshino0809@gmail.com>
2020-06-13 13:11:50 +0900

    first commmit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'swift_storageTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for swift_storageTest
+pod "Firebase/Storage"
+
+end
--- swift_storageTest/ViewController.swift
+++ swift_storageTest/ViewController.swift
@@ -5,16 +5,78 @@
 //  Created by Yuki Shinohara on 2020/06/13.
 //  Copyright © 2020 Yuki Shinohara. All rights reserved.
 //
-
+import FirebaseStorage
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    private let storage = Storage.storage().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.contentMode = .scaleAspectFit
+        
+        guard let urlString = UserDefaults.standard.value(forKey: "url") as? String, let url = URL(string: urlString) else {
+            return
+        }
+        label.text = urlString
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self.imageView.image = image
+            }
+            
+        }
+        
+        task.resume()
     }
-
-
+    @IBAction func didTapButton(_ sender: Any) {
+        let picker = UIImagePickerController() //画像を取得する
+        picker.sourceType = .photoLibrary //取得先
+        picker.delegate = self
+        picker.allowsEditing = true //取得した画像を編集するかどうか
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {return}
+        
+        guard let imageData = image.pngData() else {return}
+        
+        let ref = storage.child("images/file.png")
+        ref.putData(imageData, metadata: nil) { (_, error) in
+            guard error == nil else {
+                return
+            }
+            ref.downloadURL { (url, error) in
+                guard let url = url, error == nil else {return}
+                let urlString = url.absoluteString
+                
+                DispatchQueue.main.async {
+                    self.label.text = urlString
+                    self.imageView.image = image
+                }
+                
+                print("DownloadURL: \(urlString)")
+                UserDefaults.standard.set(urlString, forKey: "url")
+            }
+        }
+        
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
